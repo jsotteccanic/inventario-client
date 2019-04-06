0bd48ca2b76d05e53288f44ef00ed66b4db9412e
jsotteccanic <jsotteccanic@gmail.com>
2019-04-06 01:06:59 -0500

    estilo semantic

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,22 @@
 fetch('https://us-central1-inventario-app-aa28e.cloudfunctions.net/helloWorld')
-.then(x=>x.text())
-.then(x=>{
-    document.getElementById("resultado").innerHTML = x;
+    .then(x => x.text())
+    .then(x => {
+        document.getElementById("testService").innerHTML = x;
+    })
+    .catch(err => console.log(err));
+
+data = {
+    msj: "Esto es post"
+}
+fetch('https://us-central1-inventario-app-aa28e.cloudfunctions.net/helloWorld', {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
-.catch(err=> console.log(err));
+    .then(x => x.text())
+    .then(x => {
+        document.getElementById("testService").innerHTML = x;
+    })
+    .catch(err => console.log(err));
