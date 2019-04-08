ef2534beb1cd32c41a82cc98f9f8b319f1dafacf
jsotteccanic <jsotteccanic@gmail.com>
2019-04-07 21:12:29 -0500

    cambios del domingo 04.07.2019

--- public/js/index.js
+++ public/js/index.js
@@ -7,9 +7,18 @@
 //     .catch(err => alert(err));
 var db = firebase.firestore();
 
+// vista admin consulta de datos
+let opt = document.getElementById("selectColeccion");
+// registro de datos formulario de maestro de articulo
+let _presentacion = document.getElementById("_presentacion");
+let _principioActivo = document.getElementById("_principioActivo");
+let _laboratorio = document.getElementById("_laboratorio");
+let _subClase = document.getElementById("_subClase");
+let _clase = document.getElementById("_clase");
+
 firebase.auth().onAuthStateChanged(function (user) {
     if (!user) {
-        window.location.href = "./login.html";
+        window.location.href = "./index.html";
     }
 });
 
@@ -177,6 +186,179 @@ $('#proveedor')
             registrarFirebase(event.target.id, inputs);
         }
     });
+// MAESTRO DE ARTICULO
+$('#maestroDeArticulo')
+    .form({
+        fields: {
+            _codigoArticulo: {
+                identifier: '_codigoArticulo',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el codigo'
+                    }
+                ]
+            },
+            _nombreArticulo: {
+                identifier: '_nombreArticulo',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el nombre'
+                    }
+                ]
+            },
+            _multiplo: {
+                identifier: '_multiplo',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el multiplo'
+                    }
+                ]
+            },
+            _presentacion: {
+                identifier: '_presentacion',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingrese la presentación'
+                    }
+                ]
+            },
+            _costoCompra: {
+                identifier: '_costoCompra',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingrese el costo'
+                    }
+                ]
+            },
+            _principioActivo: {
+                identifier: '_principioActivo',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa  el principio activo'
+                    }
+                ]
+            },
+            _laboratorio: {
+                identifier: '_laboratorio',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el laboratorio'
+                    }
+                ]
+            },
+            _clase: {
+                identifier: '_clase',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa la clase'
+                    }
+                ]
+            },
+            _subClase: {
+                identifier: '_subClase',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa la sub clase'
+                    }
+                ]
+            },
+            _stock: {
+                identifier: '_stock',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el stock'
+                    }
+                ]
+            },
+            _lote: {
+                identifier: '_lote',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el lote'
+                    }
+                ]
+            },
+            _fechaVencimiento: {
+                identifier: '_fechaVencimiento',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa la fecha'
+                    }
+                ]
+            },
+            _codigoBarra: {
+                identifier: '_codigoBarra',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa codigo de barra'
+                    }
+                ]
+            },
+            _igv: {
+                identifier: '_igv',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el igv'
+                    }
+                ]
+            },
+            _vvf: {
+                identifier: '_vvf',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el vvf'
+                    }
+                ]
+            },
+            _pvf: {
+                identifier: '_pvf',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el pvf'
+                    }
+                ]
+            },
+            _pvp: {
+                identifier: '_pvp',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el pvp'
+                    }
+                ]
+            },
+            _incremento: {
+                identifier: '_incremento',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el incremento'
+                    }
+                ]
+            }
+        },
+        onSuccess: (event, inputs) => {
+            event.preventDefault();
+            registrarFirebase(event.target.id, inputs);
+        }
+    });
+
 // TIPO DE DOCUMENTO
 $('#tipoDocumento')
     .form({
@@ -205,13 +387,131 @@ $('#tipoDocumento')
             registrarFirebase(event.target.id, inputs);
         }
     });
-
-
-// #############
+// TIPO DE DOCUMENTO
+$('#tipoSalida')
+    .form({
+        fields: {
+            nombreTipoSalida: {
+                identifier: 'nombreTipoSalida',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el nombre del tipo de salida'
+                    }
+                ]
+            },
+            correlativoSalida: {
+                identifier: 'correlativoSalida',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el número de correlativo'
+                    }
+                ]
+            }
+        },
+        onSuccess: (event, inputs) => {
+            event.preventDefault();
+            registrarFirebase(event.target.id, inputs);
+        }
+    });
+// TIPO DE DOCUMENTO
+$('#tipoIngreso')
+    .form({
+        fields: {
+            nombreTipoIngreso: {
+                identifier: 'nombreTipoIngreso',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el nombre del tipo de ingreso'
+                    }
+                ]
+            },
+            correlativoIngreso: {
+                identifier: 'correlativoIngreso',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el número de correlativo'
+                    }
+                ]
+            }
+        },
+        onSuccess: (event, inputs) => {
+            event.preventDefault();
+            registrarFirebase(event.target.id, inputs);
+        }
+    });
+// REGISTRO DE ARTICULO
+$('#articulo')
+    .form({
+        fields: {
+            _proveedor: {
+                identifier: '_proveedor',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa un proveedor'
+                    }
+                ]
+            },
+            _tipoDocumento: {
+                identifier: '_tipoDocumento',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el tipo de documento'
+                    }
+                ]
+            },
+            _fechaRegistroArticulo: {
+                identifier: '_fechaRegistroArticulo',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa la fecha'
+                    }
+                ]
+            },
+            _numeroDocumento: {
+                identifier: '_numeroDocumento',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa el número de documento'
+                    }
+                ]
+            },
+            _almacen: {
+                identifier: '_almacen',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Ingresa un almacen'
+                    }
+                ]
+            },
+            _producto: {
+                identifier: '_producto',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Seleccione un producto'
+                    }
+                ]
+            }
+        },
+        onSuccess: (event, inputs) => {
+            event.preventDefault();
+            registrarFirebase(event.target.id, inputs);
+        }
+    });
+// Funciones
 function registrarFirebase(colecion, data) {
     db.collection(colecion).add(data)
         .then(function () {
-            alert("Document successfully written!");
+            alert("Se registor correctamente");
             $(`#${colecion}`).form('clear')
         })
         .catch(function (error) {
@@ -222,38 +522,150 @@ function registrarFirebase(colecion, data) {
 
 function obtenerDatosFirebase(e) {
     e.preventDefault();
-    let opt = document.getElementById("selectColeccion");
+
     let thead = document.getElementById("headResult");
-    thead.innerHTML="";
+    thead.innerHTML = "";
     let tbody = document.getElementById("bodyResutl");
-    tbody.innerHTML="";
+    tbody.innerHTML = "";
     let result = [];
     db.collection(opt.value).get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
-            result.push(doc.data());
+            let nDoc = doc.data();
+            nDoc.id = doc.id;
+            result.push(nDoc);
         });
         if (result.length > 0) {
             Object.keys(result[0]).forEach(x => {
-                let th = document.createElement("th");
-                th.innerText = x;
-                thead.appendChild(th);
+                if (x !== "id") {
+                    let th = document.createElement("th");
+                    th.innerText = x;
+                    thead.appendChild(th);
+                }
+
             });
         }
+        // agregando la columna de control
+        let th = document.createElement("th");
+        th.innerText = 'Control';
+        thead.appendChild(th)
+
+        //Agregando contenido a al tbody
         result.forEach(x => {
             let tr = document.createElement("tr");
-            for(valor in x){
+            tr.id = x.id;
+            for (valor in x) {
                 let td = document.createElement("td");
                 td.innerText = x[valor];
                 tr.appendChild(td);
+                if (valor == 'id') {
+                    td.innerHTML = `<i class="trash icon" onclick="eliminarRegistro(event)"></i> <i class="edit icon" onclick="editarRegistro(event)"></i>`;
+                    tbody.appendChild(tr);
+                }
             }
-            tbody.appendChild(tr);
+
         });
     });
 }
 
-// Object.keys(doc.data()).forEach(x => {
-//     let th = document.createElement('th');
-//     th.innerHTML = x;
-//     thead.appendChild(th);
-// });
-// console.log(doc.id, " => ", doc.data());
+function eliminarRegistro(e) {
+    db.collection(opt.value).doc(e.target.parentElement.parentElement.id).delete().then(function () {
+        alert("El documento se eliminó correctamente");
+        obtenerDatosFirebase(e);
+    }).catch(function (error) {
+        alert("Error removing document: ", error);
+    });
+
+}
+
+// OBTENCION DE DATOS - GET
+
+//funcion temporal para el maestro de articulo
+function cargarDatosFormularioArticulo() {
+    getDataPresentacion()
+    getDataPricipioActivo()
+    getDataLaboratorio()
+    getDataSubClase()
+    //     getDataClase()
+}
+// _presentacion
+// _principioActivo
+// _laboratorio
+// _subClase
+// _clase
+
+function getDataPresentacion() {
+    db.collection("presentacion").get().then(function (querySnapshot) {
+        _presentacion.innerHTML = "";
+        let data = [{ codPresentacion: "", nombrePresentacion: "Seleccione" }];
+        querySnapshot.forEach(function (doc) {
+            data.push(doc.data());
+        });
+        data.forEach(x => {
+            let opt = document.createElement("option");
+            opt.value = x.codPresentacion;
+            opt.innerText = x.nombrePresentacion;
+            _presentacion.appendChild(opt)
+        });
+    });
+}
+function getDataPricipioActivo() {
+    db.collection("principioActivo").get().then(function (querySnapshot) {
+        _principioActivo.innerHTML = "";
+        let data = [{ codigoPrincipioActivo: "", nombrePrincipioActivo: "Seleccione" }];
+        querySnapshot.forEach(function (doc) {
+            data.push(doc.data());
+        });
+        data.forEach(x => {
+            let opt = document.createElement("option");
+            opt.value = x.codigoPrincipioActivo;
+            opt.innerText = x.nombrePrincipioActivo;
+            _principioActivo.appendChild(opt)
+        });
+    });
+}
+function getDataLaboratorio() {
+    db.collection("laboratorio").get().then(function (querySnapshot) {
+        _laboratorio.innerHTML = "";
+        let data = [{ codigoLaboratorio: "", nombreLaboratorio: "Seleccione" }];
+        querySnapshot.forEach(function (doc) {
+            data.push(doc.data());
+        });
+        data.forEach(x => {
+            let opt = document.createElement("option");
+            opt.value = x.codigoLaboratorio;
+            opt.innerText = x.nombreLaboratorio;
+            _laboratorio.appendChild(opt)
+        });
+    });
+}
+
+// variables para la funcion getDataSubClase();
+let clase, subClase;
+function getDataSubClase() {
+    db.collection("clase").get().then(function (querySnapshot) {
+
+        _clase.innerHTML = "";
+        _subClase.innerHTML = "";
+        clase = [{ id: "", nombre: "Seleccione", subClase: { id: "", nombre: "" } }];
+        subClase = [{ id: "", nombre: "Seleccione" }];
+        querySnapshot.forEach(function (doc) {
+            clase.push(doc.data());
+            subClase.push(doc.data().subClase);
+        });
+        clase.forEach(x => {
+            let opt = document.createElement("option");
+            opt.value = x.nombre;
+            opt.innerText = x.nombre;
+            _clase.appendChild(opt)
+        });
+        subClase.forEach(x => {
+            let opt = document.createElement("option");
+            opt.value = x.id;
+            opt.innerText = x.nombre;
+            _subClase.appendChild(opt)
+        });
+    });
+}
+// function getDataClase() {
+
+// }
